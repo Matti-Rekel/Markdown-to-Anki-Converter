44dbc00523a2bf35eb3ef45ecf4087b0e7ee6283
Matti-Rekel <matti.rekel@outlook.de>
2026-08-13 14:06:49 +0200

    Auto Push

--- src/libParser/Parser.h
+++ src/libParser/Parser.h
@@ -3,19 +3,36 @@
 {
     using std::String;
 
-
     struct Card {
-        String Question;
-        String Prerequisites;
-        String Hint;
-        String Answer;
-        String Add_Information;
-        String Source;
+        string Question         = "";
+        string Prerequisites    = "";
+        string Hint             = "";
+        string Answer           = "";
+        string Add_Information  = "";
+        string Source           = "";
     };
+    enum class fields{
+        Question, Prerequisites, Hint, Answer, Add_Information, Source;
+    }
 
 
-    auto split_Text(String text) -> Card{
-        
+    auto process_file(string file_text) -> void;
+    auto split_Text(String text, std::map<string, fields> keywords) -> Card;
+    auto parse_md_to_html(string field) -> string;
+
+
+    split_Text(string text, std::map<string, fields> keywords){
+        Card res;
+
+        /* Der Plan:
+         * Die Überschrift mit ## ist die Frage. Direkt gefolgt von der Antwort.
+         * Die Zusatzfelder werden danach mit ### Makiert.
+         * In keywords ist Gespeichert, welche Wörter hinter ### welchem Feld zuzuordnen sind.
+         *
+         * Potenzielle Fehlerquelle ein Feld wird mehrfach erwähnt.
+        /*
+
+
     }
 }
 #endif 
