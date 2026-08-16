b1cdd9497f088976053c4962d0fad2b79f4d7f65
Matti-Rekel <matti.rekel@outlook.de>
2026-08-16 21:23:22 +0200

    some changes

--- src/libParser/Parser.cpp
+++ src/libParser/Parser.cpp
@@ -1,21 +1,39 @@
 #include "Parser.h"
 
+auto parser(std::string text) -> std::string{
+    std::vector<Block> blocks = parse_into_blocks(text);
+    std::cout << blocks.at(0) << std::endl;
+
+    return {};
+}
+
 auto parse_into_blocks(std::string_view text) -> std::vector<Block>{
     std::vector<Block> res;
     std::vector<std::string> lines = parse_split_lines(text);
 
-    bool paragraph = false;
     size_t index = 0;
-    while (index < lines.length()){
+    while (index < lines.size()){
 
-        if (is_beginingof_code(lines.at(index))) {
-            res.push_back(parse_codeblock(lines, index));
-            continue;
+        BlockType blocktype = begining_of_block(lines.at(index));
+
+        switch (blocktype){
+            case BlockType::Code :
+                res.push_back(parse_codeblock(lines, index));
+                break;
+            case BlockType::Math :
+                res.push_back(parse_mathblock(lines, index));
+                break;
+            case BlockType::List :
+                res.push_back(parse_listblock(lines, index));
+                break;
+            default :
+                res.push_back(parse_paragraph(lines, index));
+                break;
         }
     }
 
 
-    return {};
+    return res;
 }
 
 auto parse_split_lines(std::string_view text) -> std::vector<std::string>{
@@ -33,16 +51,17 @@ auto parse_split_lines(std::string_view text) -> std::vector<std::string>{
     return res;
 }
 
+// --- Parsing individual Blocks ---
+
 auto parse_codeblock(std::vector<std::string> lines, size_t & index) -> Block{
     Block result;
     result.type = BlockType::Code;
 
-    // Skip first line
     index++;
 
     while(index < lines.size()){
 
-        if(is_endof_code(lines.at(index))){
+        if(end_of_codeblock(lines.at(index))){
             index++;
             break;
         }
@@ -52,22 +71,11 @@ auto parse_codeblock(std::vector<std::string> lines, size_t & index) -> Block{
 
         result.content += lines[index];
 
-        ++index;
+        index++;
     }
     return result;
 }
-auto is_beginingof_code(std::string line) -> bool{
-    if (line.size() > 2 && (line.at(0) == '`' && line.at(1) == '`' && line.at(2) == '`')) {
-        return true;
-    }
-    return false;
-}
-auto is_endof_code(std::string line) -> bool{
-    if (line.size() > 2 && (line.at(0) == '`' && line.at(1) == '`' && line.at(2) == '`')) {
-        return true;
-    }
-    return false;
-}
+
 
 auto parse_mathblock(std::vector<std::string> lines, size_t & index) -> Block{
     Block result;
@@ -76,11 +84,111 @@ auto parse_mathblock(std::vector<std::string> lines, size_t & index) -> Block{
     index++;
 
     while(index < lines.size()){
-        if(is_endof_math(lines.at(index))){
+        if(end_of_mathblock(lines.at(index))){
             index++;
             break;
         }
 
+        result.content += lines[index];
 
+        index++;
     }
+    return result;
+}
+
+
+auto parse_listblock(std::vector<std::string> lines, size_t & index) -> Block{
+    Block result;
+    result.type = BlockType::List;
+
+    while(index < lines.size()){
+        if(end_of_listblock(lines.at(index))){
+            index++;
+            break;
+        }
+
+        result.content += lines[index] + '\n';
+
+        index++;
+    }
+    return result;
+}
+
+auto parse_paragraph(std::vector<std::string> lines, size_t& index) -> Block {
+    Block result;
+    result.type = BlockType::Paragraph;
+
+    while (index < lines.size() &&
+           begining_of_block(lines.at(index)) == BlockType::Paragraph) {
+
+        if (!result.content.empty())
+            result.content += '\n';
+
+        result.content += lines.at(index);
+        index++;
+    }
+
+    return result;
+}
+
+// --- Checking for Begining of Blocks ---
+
+auto begining_of_block(std::string line) -> BlockType{
+    if(begining_of_codeblock(line)){
+        return BlockType::Code;
+    }
+    if(begining_of_mathblock(line)){
+        return BlockType::Math;
+    }
+    if(begining_of_listblock(line)){
+        return BlockType::List;
+    }
+    else{
+        return BlockType::Paragraph;
+    }
+}
+
+
+auto begining_of_codeblock(std::string line) -> bool{
+    if (line.size() > 2 && (line.at(0) == '`' && line.at(1) == '`' && line.at(2) == '`')) {
+        return true;
+    }
+    return false;
+}
+
+auto begining_of_mathblock(std::string line) -> bool{
+    if (line.size() > 1 && (line.at(0) == '$' && line.at(1) == '$')){
+        return true;
+    }
+    return false;
+}
+
+auto begining_of_listblock(std::string line) -> bool{
+    if(line.size() > 1 && (line.at(0) == '-' && line.at(1) == ' ')){
+        return true;
+    }
+    return false;
+}
+
+// --- Checking for End of Blocks ---
+
+auto end_of_codeblock(std::string line) -> bool{
+    if (line.size() > 2 && (line.at(0) == '`' && line.at(1) == '`' && line.at(2) == '`')) {
+        return true;
+    }
+    return false;
+}
+
+auto end_of_mathblock(std::string line) -> bool{
+    if (line.size() > 1 && (line.at(0) == '$' && line.at(1) == '$')){
+        return true;
+    }
+    return false;
+}
+
+auto end_of_lineblock(std::string line) -> bool{
+    if (line.empty()){
+        return true;
+    }
+    return false;    
 }
--- src/libParser/Parser.h
+++ src/libParser/Parser.h
@@ -4,22 +4,66 @@
 #include<string>
 #include<vector>
 
+// --- Structs and Enums ---
 
 enum class BlockType {
     Paragraph,
-    Heading,
     Code,
     Math,
     List,
-    Quote,
-    Table,
-    ThematicBreak
 };
 
 struct Block {
     BlockType type;
     std::string content;
 };
+
+
+
+auto parser(std::string text) -> std::string;
+
+// Block Parser 
+
+auto parse_into_blocks  (std::string & text) -> std::vector<Block>;
+auto parse_split_lines(std::string_view text) -> std::vector<std::string>;
+
+auto begining_of_block(std::string line) -> BlockType;
+
+auto parse_codeblock    (std::vector<std::string> lines, size_t & index) -> Block;
+    auto begining_of_codeblock   (std::string line)   -> bool;
+    auto end_of_codeblock        (std::string line)   -> bool;
+auto parse_mathblock    (std::vector<std::string> lines, size_t & index) -> Block;
+    auto begining_of_mathblock   (std::string line)   -> bool;
+    auto end_of_mathblock        (std::string line)   -> bool;
+auto parse_listblock    (std::vector<std::string> lines, size_t & index) -> Block;
+    auto begining_of_listblock   (std::string line)   -> bool;
+    auto end_of_listblock        (std::string line)   -> bool;
+
+auto parse_paragraph(std::vector<std::string> lines, size_t& index) -> Block;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 enum class InlineType {
@@ -41,8 +85,6 @@ struct Inline {
 };
 
 
-auto parser(std::string text) -> std::string;
-
 // Line Parser
 
 auto parse_inlines(std::string_view text) -> std::vector<Inline>;
@@ -55,21 +97,6 @@ auto parse_cloze        (std::string & text) -> void;
 auto parse_image        (std::string & text) -> void;
 auto parse_link         (std::string & text) -> void;
 auto parse_charakters   (std::string & text) -> void;
-
-// Block Parser 
-
-auto parse_into_blocks  (std::string & text) -> std::vector<Block>;
-auto parse_split_lines(std::string_view text) -> std::vector<std::string>;
-
-auto parse_codeblock    (std::vector<std::string> lines, size_t & index) -> Block;
-    auto is_beginingof_code(std::string line)   -> bool;
-    auto is_endof_code(std::string line)        -> bool;
-auto parse_mathblock    (std::string & text) -> void;
-auto parse_list         (std::string & text) -> void;
-auto parse_quote        (std::string & text) -> void;
-auto parse_table        (std::string & text) -> void;
-
-
 
 
 #endif
