12519c3d31131a2639803de1898f8a8b2901ff8d
Matti-Rekel <matti.rekel@outlook.de>
2026-09-21 10:31:13 +0200

    Auto Push

--- include/visitor/AnkiRenderer.h
+++ include/visitor/AnkiRenderer.h
@@ -10,7 +10,8 @@
 enum class FieldType
 {
     Question,
-    Answer
+    Answer,
+    Cloze
 };
 enum class CardType
 {
@@ -20,8 +21,6 @@ enum class CardType
 class Field
 {
   public:
-    static auto determine_fieldType(Field const& field) -> FieldType;
-
     std::vector<Block const*> children;
     FieldType fieldType;
 };
@@ -29,17 +28,25 @@ class Field
 class Card
 {
   public:
-    static auto determine_cardType(Card const& card) -> CardType;
-
     std::vector<Field> fields;
     CardType cardType;
+};
+
+class CardParser
+{
+    static auto split_into_cards(Document const& node) -> std::vector<Card>;
+    static auto consume_card(Document const& document, size_t& i) -> Card;
+
+    static auto determine_cardType(Card const& card) -> CardType;
+    static auto is_cloze_card(Card const& card) -> bool;
+
+    static auto determine_fieldType(Field const& field) -> FieldType;
 };
 
 class AnkiRenderer : public Visitor
 {
   public:
     void render(Document const& document);
-    auto split_into_cards(Document const& node) -> std::vector<Card>;
     auto render_card(Card const& card) -> std::string;
 
     void visit(Paragraph& node) override;
--- src/visitor/AnkiRenderer.cpp
+++ src/visitor/AnkiRenderer.cpp
@@ -5,77 +5,95 @@
 #include "ast/Inline.h"
 #include <vector>
 
-auto Field::determine_fieldType(const Field& field) -> FieldType
+auto CardParser::determine_fieldType(const Field& field) -> FieldType
 {
     // TODO
     return {};
 }
-auto Card::determine_cardType(const Card& card) -> CardType
+auto CardParser::determine_cardType(const Card& card) -> CardType
 {
+    for (auto field : card.fields)
+    {
+        field.fieldType = CardParser::determine_fieldType(field);
+    }
     // TODO
-    return {};
+
+    if (is_cloze_card(card))
+        return CardType::Cloze;
+
+    return CardType::Basic;
 }
+auto is_cloze_card(Card const& card) -> bool { return card.fields.at(2).fieldType == FieldType::Cloze; }
 
 void AnkiRenderer::render(Document const& node)
 {
-    auto cards = split_into_cards(node);
+    auto cards = CardParser::split_into_cards(node);
 
     for (auto& card : cards)
     {
-        card.cardType = Card::determine_cardType(card);
+        card.cardType = CardParser::determine_cardType(card);
 
         output += render_card(card);
     }
 }
 
-auto AnkiRenderer::split_into_cards(Document const& document) -> std::vector<Card>
+auto CardParser::split_into_cards(Document const& document) -> std::vector<Card>
 {
     std::vector<Card> result;
 
     for (std::size_t i = 0; i < document.children.size(); ++i)
     {
-        Card card;
-
         auto* cardHeading = dynamic_cast<Heading const*>(document.children[i].get());
 
         if (cardHeading && cardHeading->level == 2)
-        {
-            Field question;
-            question.children.push_back(document.children[i].get());
-            card.fields.push_back(std::move(question));
-
-            ++i;
-
-            while (i < document.children.size())
-            {
-                auto* heading = dynamic_cast<Heading const*>(document.children[i].get());
-
-                if (heading && heading->level == 2)
-                {
-                    --i;
-                    break;
-                }
-
-                if (heading && heading->level == 3)
-                {
-                    Field field;
-                    field.children.push_back(document.children[i].get());
-                    card.fields.push_back(std::move(field));
-                }
-                else
-                {
-                    card.fields.back().children.push_back(document.children[i].get());
-                }
-
-                ++i;
-            }
-
-            result.push_back(std::move(card));
-        }
+            result.push_back(consume_card(document, i));
     }
 
     return result;
 }
+
+auto AnkiRenderer::consume_card(Document const& document, std::size_t& i) -> Card
+{
+    Card result;
+
+    // The ## heading starts the question field.
+    Field field;
+    field.children.push_back(document.children.at(i).get());
+    result.fields.push_back(std::move(field));
+
+    ++i;
+
+    while (i < document.children.size())
+    {
+        // TODO currently the Answer will be part of the question field
+        auto* heading = dynamic_cast<Heading const*>(document.children[i].get());
+
+        // We reached the beginning of the next card.
+        if (heading && heading->level == 2)
+        {
+            --i;
+            break;
+        }
+
+        // We reached the beginning of a new field.
+        if (heading && heading->level == 3)
+        {
+            Field newField;
+            newField.children.push_back(document.children[i].get());
+            result.fields.push_back(std::move(newField));
+        }
+        else
+        {
+            // Add the node to the current field.
+            result.fields.back().children.push_back(document.children[i].get());
+        }
+
+        ++i;
+    }
+
+    return result;
+}
+
 auto AnkiRenderer::render_card(Card const& card) -> std::string
 {
     // TODO
